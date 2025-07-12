64e44d9634c5c67caede939ea5bb8e9a5c6c9249
Angela Qian <angelazqian@gmail.com>
2025-07-12 13:55:01 -0700

    ae linter issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,4 @@
 import Link from 'next/link';
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export default function Home() {
   return (
