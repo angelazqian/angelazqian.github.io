240b2d58ac5c340b799c50b20a65b704509d16d7
Angela Qian <angelazqian@gmail.com>
2025-07-12 13:55:01 -0700

    fixing stuff

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 const repo = 'angelazqian.github.io';
 module.exports = {
   output: 'export',
-  basePath: '/' + repo,
-  assetPrefix: '/' + repo + '/',
+  basePath: isProd ? '/' + repo : '',
+  assetPrefix: isProd ? '/' + repo + '/' : '',
 };
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  basePath: isProd ? '/' + repo : '',
+  assetPrefix: isProd ? '/' + repo + '/' : '',
 };
 
 export default nextConfig;
