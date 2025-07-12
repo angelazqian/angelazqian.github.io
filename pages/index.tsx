1bceaf8eb0251fff4f8d4f48d4e2b381ac88ce26
Angela Qian <angelazqian@gmail.com>
2025-07-12 13:55:02 -0700

    add resume and project list

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,14 @@ export default function Home() {
             <h1 className="font-bold text-7xl underline" style={{ textDecorationColor: '#880000' }} >angela qian</h1>
             <img src="https://avatars.githubusercontent.com/u/107577606" className="w-[140px] h-[140px] rounded-full object-cover relative top-[-20px] ml-[30px] "/>
           </div>
-          <p className="text-xl mb-[10px] ">I am a sophomore at Purdue University pursuing a major in Computer Science and a minor in Mathematics. </p>
-          <p className="text-xl">In my free time, you can catch me drawing, reading, or playing piano.</p>
+          <p className="text-lg mb-[10px]">I am a sophomore at Purdue University pursuing a major in Computer Science and a minor in Mathematics. </p>
+          <p className="text-lg mb-[10px]">In my free time, you can catch me drawing, reading, or playing piano.</p>
+          <p className="text-lg">My resume can be found {' '}
+            <a href="/resume.pdf" className="text-blue-600 underline">
+                here</a>.
+          </p>
         </div>
-        <Link href="/projects" style={{ color: 'blue', textDecoration: 'underline' }}>
+        <Link href="/projects" className="text-blue-600 underline">
           See my projects
         </Link>
       </header>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,14 +4,16 @@ export default function Projects() {
   return (
     <div style={{ maxWidth: "600px", margin: "2rem auto", fontFamily: "sans-serif" }}>
       <h1>Projects</h1>
-      <p>Here&apos;s a list of some things I&apos;ve built.</p>
+      <p>Here&apos;s a list of some things I&apos;ve made</p>
       <ul>
-        <li>Project One</li>
-        <li>Project Two</li>
+        <li>2048 AI Player</li>
+        <li>Eyesore</li>
+        <li>Easy Mode 2048</li>
+        <li>ScribbleScore</li>
+        <li>Facial Recognition Door Lock</li>
       </ul>
-      <Link href="/" style={{ color: "blue", textDecoration: "underline" }}>
-        Back to home
-      </Link>
+      <p><a href="/" className="text-blue-600 underline">
+      Go back home</a></p>
     </div>
   );
 }
