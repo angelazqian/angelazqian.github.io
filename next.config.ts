01d7c908be93bf8413eb573ab5b75f5ddb9a9433
Angela Qian <angelazqian@gmail.com>
2025-07-12 13:55:01 -0700

    finally got gh-pages working

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
-const repo = 'angelazqian.github.io';
-module.exports = {
-  output: 'export',
-  basePath: isProd ? '/' + repo : '',
-  assetPrefix: isProd ? '/' + repo + '/' : '',
-};
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  output: 'export',
-  basePath: isProd ? '/' + repo : '',
-  assetPrefix: isProd ? '/' + repo + '/' : '',
+  output: 'export'
 };
 
 export default nextConfig;
