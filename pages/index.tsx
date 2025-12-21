fe748311fb11d1fef267dc8dc7ade4129d79a35d
Angela Qian <angelazqian@gmail.com>
2025-12-20 21:15:50 -0800

    add sighorse, change favicon, update projects page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,14 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs';
 // import { FaEnvelope, FaArtstation} from 'react-icons/fa6';
 import { FaEnvelope} from 'react-icons/fa6';
 import ConwayBackground from '../components/conwaybackground';
+import Head from "next/head";
 
 export default function Home() {
   return (
     <><ConwayBackground />
+    <Head>
+      <title>Angela Qian</title>
+    </Head>
     <div className="App h-screen overflow-hidden">
       <header className='App-header'>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jpswalsh/academicons@1/css/academicons.min.css" />
@@ -17,6 +21,7 @@ export default function Home() {
             </div>
             <p className="text-lg mb-[10px]">I am a junior at Purdue University pursuing a major in Computer Science and a minor in Mathematics. </p>
             <p className="text-lg mb-[10px]">In my free time, you can catch me drawing, reading, or playing piano.</p>
+            {/* <p className="text-lg mb-[10px]">In my free time, you can catch me wandering deep into the forest, throwing rocks at people I don't like, and chasing women down dark alleyways.</p> */}
             <p className="text-lg">My resume can be found {' '}
               <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
                 here</a>.
@@ -37,7 +42,7 @@ export default function Home() {
             <FaEnvelope />
           </a>
           <a href="https://scholar.google.com/citations?user=N9x8yXoAAAAJ&hl=en" target="_blank" rel="noopener noreferrer" className="text-inherit transition-transform duration-150 hover:scale-125">
-            <i className="ai ai-google-scholar text-[55px] inline-block align-middle mr-[-10px] leading-none"></i>
+            <i className="ai ai-google-scholar text-[55px] inline-block align-middle leading-none" style={{ transform: "translateX(-10px) translateY(-1px)" }}></i>
           </a>
           {/* <a href="https://toaster_drips.artstation.com/" target="_blank" rel="noopener noreferrer" className="text-inherit transition-transform duration-150 hover:scale-125">
             <FaArtstation />
